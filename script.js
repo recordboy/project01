214973e1400abc657138ff1f10039d1216c1979c
recordboy <ksj_design@naver.com>
2022-06-13 21:04:03 +0900

    [기능] 엘레베이터 이동 요소 수정 class 추가 방식 >> 요소를 한개 더 만들어 position 으로 띄운뒤 이동시키는 방식으로 변경

--- script.js
+++ script.js
@@ -16,20 +16,20 @@ function addEvent() {
     const btnCreate = document.getElementById('btn_create');
     const btnElevator = document.getElementById('elevator_btn');
 
-
-
     btnCreate.addEventListener('click', function (e) {
         let data = getElevatorData();
         createElevator(data);
     });
-
 
     btnElevator.addEventListener('click', function (e) {
         let btnId = e.target.id;
         let btnIdx = Number(btnId.substring(9));
         moveElevator(btnIdx);
     });
-    
+
+    // 테스트
+    let data = getElevatorData();
+    createElevator(data);
 
 }
 
@@ -57,10 +57,11 @@ function createElevator(data) {
     const elevator = document.getElementById('elevator');
     const elevatorBtn = document.getElementById('elevator_btn');
 
-    var tower = null;
-    var towerFloor = null;
-    var towerBtn = null;
-    var towerBtnTxt = '';
+    let tower = null;
+    let towerFloor = null;
+    let towerBtn = null;
+    let towerBtnTxt = '';
+    let room = null;
 
     // 엘레베이터 버튼 생성
     for (let i = data.numberVal; i > 0; i--) {
@@ -73,39 +74,47 @@ function createElevator(data) {
         elevatorBtn.appendChild(towerBtn);
     }
 
-    // 엘레베이터 생성
+    // 각 엘레베이터 타워 생성
     for (let i = 0; i < data.numberVal; i++) {
 
-        // 엘레베이터 생성
+        // 엘레베이터 층 생성
         tower = document.createElement('div');
         tower.classList = 'tower';
         elevator.appendChild(tower);
-
         for (let j = data.floorVal; j > 0; j--) {
             towerFloor = document.createElement('div');
 
             // 각 층 값 넣기
             if (j === 1) {
                 global.push(j);
-                towerFloor.classList = 'on';
             }
             tower.appendChild(towerFloor);
         }
+
+        // 엘레베이터 룸 생성
+        room = document.createElement('div');
+        room.classList = 'room';
+        tower.appendChild(room);
     }
 }
 
 
 function moveElevator(idx) {
 
+    // 엘레베이터 객체
     const elevator = document.getElementById('elevator');
-    const elevatorChilds = elevator.childNodes;
-    
+    const elevatorTower = elevator.childNodes;
 
-    for (let i = 0; i < elevatorChilds.length; i++) {
-        
+    for (let i = 0; i < elevatorTower.length; i++) {
+
+
+        if (global[i] !== idx) {
+            moveAnimate(elevatorTower[i], idx);
+            return;
+        }
     }
 }
 
-
-
-
+function moveAnimate(target, idx) {
+    debugger
+}
